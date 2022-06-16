5609b34839771f7d22e1478a561273dd14d52a6b
jesusmirena <jesusamh16@gmail.com>
2022-06-15 22:07:45 -0500

    Get Pokemons Ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,48 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Form from "./components/Form";
+import SearchBar from "./components/SearchBar";
+import Table from "./components/Table";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemons, setPokemons] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getPokemons = async () => {
+    const response = await axios.get(
+      "https://bp-pokemons.herokuapp.com/?idAuthor=1"
+    );
+    setPokemons(response.data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+    getPokemons();
+  }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <h1>hola</h1>
+      {!isLoading ? (
+        <>
+          <SearchBar
+            searchValue={searchValue}
+            setSearchValue={setSearchValue}
+          />
+          <Form />
+          <Table
+            pokemons={pokemons}
+            setPokemons={setPokemons}
+            searchValue={searchValue}
+          />
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
